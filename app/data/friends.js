0975aa0ad2479509c66d2ac76ac43cdc39fc3e26
femioye <johnytrip4u@yahoo.com>
2018-05-15 23:24:35 -0500

    more commits

---
+++ app/data/friends.js
@@ -0,0 +1,114 @@
+
+let friendArray = [
+    {
+        friendName: "Donald",
+        photo: "https://media.giphy.com/media/1c5yGrh42XRja/giphy.gif",
+        scores: [
+            "3",
+            "6",
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "4",
+            "2",
+            "6",
+            "3"
+        ]
+    },
+    {
+        friendName: "Precious",
+        photo: "https://media.giphy.com/media/31lPv5L3aIvTi/giphy.gif",
+        scores: [
+            "1",
+            "5",
+            "6",
+            "4",
+            "3",
+            "2",
+            "4",
+            "2",
+            "5",
+            "3",
+            "1",
+            "6"
+        ]
+    },
+    {
+        friendName: "Diamond",
+        photo: "https://media.giphy.com/media/jabxUOlPaXYe4/giphy.gif",
+        scores: [
+            "5",
+            "4",
+            "6",
+            "1",
+            "3",
+            "4",
+            "1",
+            "5",
+            "6",
+            "1",
+            "2",
+            "4"
+        ]
+    },
+    {
+        friendName: "Leslie",
+        photo: "https://media.giphy.com/media/pPr0e5tqtCwTe/giphy.gif",
+        scores: [
+            "1",
+            "6",
+            "4",
+            "5",
+            "3",
+            "6",
+            "3",
+            "1",
+            "5",
+            "2",
+            "1",
+            "6"
+        ]
+    },
+    {
+        friendName: "Herietta",
+        photo: "https://media.giphy.com/media/3o6Mpqp0h5uJcowIRW/source.gif",
+        scores: [
+            "2",
+            "5",
+            "2",
+            "6",
+            "1",
+            "5",
+            "6",
+            "3",
+            "2",
+            "5",
+            "6",
+            "1",
+            "4"
+        ]
+    },
+    {
+        friendName: "Doggy",
+        photo: "https://media.giphy.com/media/oji893UA1Zuus/giphy.gif",
+        scores: [
+            "6",
+            "5",
+            "3",
+            "2",
+            "4",
+            "6",
+            "3",
+            "4",
+            "5",
+            "6",
+            "5",
+            "1",
+            "3"
+        ]
+    }
+];
+
+module.exports = friendArray;
